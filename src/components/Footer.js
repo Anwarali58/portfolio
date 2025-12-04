365e30ed4f46150d9fc85c7dd6fdccb224e5a9d1
anwar ali <anwaralishigri58@gmail.com>
2025-12-04 18:38:49 +0500

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="container container-lg">
         <small style={{color: 'var(--muted)'}}>
-          © {new Date().getFullYear()} Zulqarnain • Built with passion
+          © {new Date().getFullYear()} Anwar shigri • Built with passion
         </small>
       </div>
     </footer>
